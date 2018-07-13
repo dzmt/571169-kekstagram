e2531d59a58ce84754b84da4fc2ee6e51d21388e
Dzmitry <dzmitrySmir@gmail.com>
2018-07-13 23:00:05 +0300

    show choosen file for editing and uploading

--- js/upload.js
+++ js/upload.js
@@ -16,9 +16,14 @@
   var SELECTOR_IMG_UPLOAD_INPUT = '.img-upload__input';
   var SELECTOR_IMG_UPLOAD_CANCEL = '.img-upload__cancel';
   var SELECTOR_FORM_UPLOAD = '#upload-select-image';
+  var IMG_PREVIEW = '.img-upload__preview img';
 
   var IMG_UPLOAD_OVERLAY = document.querySelector(SELECTOR_IMG_UPLOAD_OVERLAY);
   var IMG_UPLOAD_PREVIEW = IMG_UPLOAD_OVERLAY.querySelector(SELECTOR_IMG_UPLOAD_PREVIEW);
+
+  var FILE_READER_EVENT_LOAD = 'load';
+
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
   var EVENT = window.enum.EVENT;
 
@@ -61,8 +66,32 @@
     window.form.removeEventListener();
   };
 
-  var imgUploadInputChangeHandler = function () {
+  var showChoosenImg = function (file) {
+    var reader = new FileReader();
+
+    reader.addEventListener(FILE_READER_EVENT_LOAD, function (evt) {
+      var preview = document.querySelector(IMG_PREVIEW);
+      preview.src = evt.target.result;
+    });
+
+    reader.readAsDataURL(file);
+  };
+
+  var matchTypeFile = function (fileName, types) {
+    fileName = fileName.toLowerCase();
+    var match = types.some(function (type) {
+      return fileName.endsWith(type);
+    });
+    return match;
+  };
+
+  var imgUploadInputChangeHandler = function (evt) {
     openImgUploadOverlay();
+
+    var file = evt.target.files[0];
+    if (matchTypeFile(file.name, FILE_TYPES)) {
+      showChoosenImg(file);
+    }
   };
 
   var imgUploadCancelClickHandler = function () {
@@ -74,7 +103,6 @@
 
   var imgUploadCancel = document.querySelector(SELECTOR_IMG_UPLOAD_CANCEL);
   imgUploadCancel.addEventListener(EVENT.CLICK, imgUploadCancelClickHandler);
-
 
   window.upload = {
     close: function () {
